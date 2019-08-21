b37282827c68a699d34a7d5c96911a0c8dc4d142
Roman <vazhnik.roma@mail.ru>
2019-08-21 10:17:47 +0300

    Сделал основную логику и

---
+++ HW_9/Animations+Curves data.swift
@@ -0,0 +1,46 @@
+//
+//  Animations+Curves data.swift
+//  HW_9
+//
+//  Created by Роман Важник on 20/08/2019.
+//  Copyright © 2019 Роман Важник. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+let animations = [
+    "shake",
+    "pop",
+    "morph",
+    "squeeze",
+    "wobble",
+    "swing",
+    "flipX",
+    "flipY",
+    "fall",
+    "squeezeLeft",
+    "squeezeRight",
+    "squeezeDown",
+    "squeezeUp",
+    "slideLeft",
+    "slideRight",
+    "slideDown",
+    "slideUp",
+    "fadeIn",
+    "fadeInLeft",
+    "fadeInRight",
+    "fadeInDown",
+    "fadeInUp",
+    "zoomIn",
+    "flash"
+]
+
+let curves = [
+    "spring",
+    "linear",
+    "easeIn",
+    "easeOut",
+    "easeInOut",
+]
--- HW_9/ViewController.swift
+++ HW_9/ViewController.swift
@@ -6,15 +6,45 @@
 //  Copyright © 2019 Роман Важник. All rights reserved.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var nextAnimationButton: UIButton!
+    @IBOutlet var animatedView: SpringView!
+    @IBOutlet var animationName: SpringLabel!
+    
+    var nextAnimation = animations.randomElement()
+    var nextCurve = curves.randomElement()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        nextAnimationButton.setTitle("\(nextAnimation!) + \(nextCurve!)", for: .normal)
+        
+        animatedViewSetup()
+        
+    }
+    
+    func animatedViewSetup() {
+        animatedView.duration = 1
+        animatedView.force = 1
+        animatedView.delay = 0
     }
 
-
+    @IBAction func nextAnimationButtonPressed() {
+        
+        animationName.text = "This animation was (\(nextAnimation!) + \(nextCurve!))"
+        animatedView.animation = nextAnimation!
+        animatedView.curve = nextCurve!
+        animatedView.animate()
+        
+        nextAnimation = animations.randomElement()
+        nextCurve = curves.randomElement()
+        
+        nextAnimationButton.setTitle("\(nextAnimation!) + \(nextCurve!)", for: .normal)
+        
+    }
+    
 }
 
